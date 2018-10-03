543bb13be4b4e10b537cb1ceaebeb44388e823cf
kiran irshad <you@example.com>
2018-10-04 01:15:47 +0500

    my commit

---
+++ script.js
@@ -0,0 +1,96 @@
+let celebrities = [
+	{	
+		id: 1,
+		name: "Abdul Sattar Edhi",
+		title: "Founder, Edhi Foundation",
+		readMoreUrl: "https://en.wikipedia.org/wiki/Abdul_Sattar_Edhi",
+		imgURL: "abdul-sattar.JPG",
+		quote: "No religion is higher than Humanity"
+	},
+
+	{
+		id: 2,
+		name: "Malala Yousufzai",
+		title: "Female Education Activities",
+		readMoreUrl: "https://en.wikipedia.org/wiki/Malala_Yousafzai",
+		imgURL: "malala.jpg",
+		quote: "One child, one teacher, one book, one pen can chnage the world"
+	},
+
+	{
+		id: 3,
+		name: "Abdul Qadeer Khan",
+		title: "Nuclear Physiciat",
+		readMoreUrl: "https://en.wikipedia.org/wiki/Abdul_Qadeer_Khan",
+		imgURL: "abdul-qadeer.jpg",
+		quote: "I am proud of my work for my country"
+	},
+
+	{
+		id: 4,
+		name: "Quaid-e-Azam",
+		title: "Mohammad Ali Jinnah",
+		readMoreUrl: "https://en.wikipedia.org/wiki/Muhammad_Ali_Jinnah",
+		imgURL: "quaid-e-azam.jpg",
+		quote: "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve."
+	},
+
+	{
+		id: 5,
+		name: "Arfa Karim",
+		title: "Computer Prodigy",
+		readMoreUrl: "https://en.wikipedia.org/wiki/Arfa_Karim",
+		imgURL: "arfa.jpg",
+		quote: "If you think shy, you act shy. If you think confident you act confident."
+	},
+
+	{
+		id: 6,
+		name: "Diana",
+		title: "Princess of Wales",
+		readMoreUrl: "https://en.wikipedia.org/wiki/Diana,_Princess_of_Wales",
+		imgURL: "diana.jpg",
+		quote: "Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you."
+	},
+
+
+	{
+		id: 7,
+		name: "Elon Musk",
+		title: "Founder, SpaceX",
+		readMoreUrl: "https://en.wikipedia.org/wiki/Elon_Musk",
+		imgURL: "elon.jpg",
+		quote: "I think it is possible for ordinary people to choose to be extraordinary."
+	},
+ ]
+	
+
+function initialize() {
+	// console.log("assad")
+	let celebritiesGroupEl = document.querySelector("#celebritries-List");
+	for(let i = 0; i < celebrities.length; i ++){
+		// console.log(celebrities[i])
+	
+		celebritiesGroupEl.innerHTML += `
+					<div class="celebrity-img-div" 
+					  style="background-image: url(images/${celebrities[i].imgURL})"
+					  onclick="changeCeleb(${i})">
+					  <p class="celebrity-img-text">${celebrities[i].name}</p>
+				    </div>`
+	}
+	changeCeleb(0);
+}
+
+function changeCeleb(celeIndex){
+	document.querySelector("#celebrity-name").innerHTML = celebrities[celeIndex].name;
+	document.querySelector("#celebrity-title").innerHTML = celebrities[celeIndex].title;
+	document.querySelector("#celebrity-img").style["background-image"] = `url(images/${celebrities[celeIndex].imgURL})`;
+	document.querySelector("#celebrity-quote").innerHTML = celebrities[celeIndex].quote;
+	document.querySelector("#celebrity-link a").href = celebrities[celeIndex].readMoreUrl;
+	
+	
+
+}
+
+
+
